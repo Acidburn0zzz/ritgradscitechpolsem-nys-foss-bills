5d189cfa1c609ebddd17ef3a29a96749d0ce7f28
RemyD <remyd@civx.us>
2013-02-10 00:44:29 -0500

    Added printable html pages to textify.sh script to be converted to plain text

--- textify.sh
+++ textify.sh
@@ -5,3 +5,8 @@ pdf2txt.py a8682-2011.pdf > texts/a8682-2011.txt
 pdf2txt.py a8831-2012.pdf > texts/a8831-2012.txt
 pdf2txt.py a3158-2013.pdf > texts/a3158-2013.txt
 pdf2txt.py s00173-2013.pdf > texts/s00173-2013.txt
+pdf2txt.py a03158-2013-html.pdf > texts/a03158-2013-html.txt
+pdf2txt.py a08682-2011-html.pdf > texts/a08682-2011-html.txt
+pdf2txt.py s04046-2009-html.pdf > texts/s04046-2009-html.txt
+pdf2txt.py s0173-2013-html.pdf > texts/s0173-2013-html.txt
+pdf2txt.py a08831-2012-html.pdf > texts/a08831-2012-html.txt
