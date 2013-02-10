d149f4ff3e304c293139b475e60c4e93917b0185
RemyD <remyd@civx.us>
2013-02-09 21:56:56 -0500

    pdf2txt script; convert all the bill .pdf files to .txt

---
+++ textify.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pdf2txt.py s4046-2009.pdf > texts/s4046-2009.txt
+pdf2txt.py a8682-2011.pdf > texts/a8682-2011.txt
+pdf2txt.py a8831-2012.pdf > texts/a8831-2012.txt
+pdf2txt.py a3158-2013.pdf > texts/a3158-2013.txt
+pdf2txt.py s00173-2013.pdf > texts/s00173-2013.txt
